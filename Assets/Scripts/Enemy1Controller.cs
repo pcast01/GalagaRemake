b654d7c1ccb535145f0244496707c6c99924b7b5
Jonathansty <jonathan.steyfkens@telenet.be>
2015-09-16 21:17:58 +0200

    Changed usage of iTween method for enemy1Controller

--- Assets/Scripts/Enemy1Controller.cs
+++ Assets/Scripts/Enemy1Controller.cs
@@ -16,11 +16,12 @@ public class Enemy1Controller : EnemyController {
 
     private List<Vector3> _waypoints;
     private Vector3 _originalPosition;
-    private float _pathPercentage = 0f;
     private float _remainingFallDelay = 5f;
 
     private bool _isOnPath = false;
     private bool _finishedPath = false;
+
+    private Hashtable _myTween = new Hashtable();
     void Awake() {
         // Save the first transform so we can use that to create our path.
         _originalPosition = transform.position;
@@ -29,7 +30,7 @@ public class Enemy1Controller : EnemyController {
     }
 	void Start () {
         base.Start();
-        
+
         
 	}
 	
@@ -45,69 +46,63 @@ public class Enemy1Controller : EnemyController {
             _originalPosition = transform.position;
             CreatePath();
             _isOnPath = !_isOnPath;
+            iTween.MoveTo(gameObject,_myTween);
             
-        }
-        if (_isOnPath)
-        {
-            _pathPercentage += Time.deltaTime * pathSpeed / 10;
-            if (_pathPercentage > 1)
-            {
-                _pathPercentage = 0;
-                if (_finishedPath)
-                {
-                    _isOnPath = false;
-                    _waypoints.Clear();
-                }
-                _finishedPath = true;
-                
-                CreateIncomingPath();
-                transform.position = _originalPosition;
-                
-            }
-            if (gameObject)
-            {
-                if (_finishedPath)
-                {
-                    iTween.PutOnPath(gameObject, _waypoints.ToArray(), _pathPercentage);
-                }
-                else
-                {
-                    iTween.PutOnPath(gameObject, _waypoints.ToArray(), _pathPercentage);  
-                }
-                
-            }
-            
-        }
-        
-
-        
+        }   
 	}
     private void CreatePath()
     {
+        
         //_originalTransform.position = transform.position;
         _waypoints.Clear();
         _waypoints.Add(transform.position);
-        for (float i = 0; i < 1; i+= 0.2f)
-        {
-            Vector3 lerpPoint = Vector3.Lerp(transform.position, playerOffsetPoints[0].position, i);
-            _waypoints.Add(lerpPoint);
-        }
-        
-       
+
+        _waypoints.Add(Vector3.Lerp(transform.position, playerOffsetPoints[0].position, 0.8f));
         foreach (var thisTransform in playerOffsetPoints)
         {
             _waypoints.Add(thisTransform.position);
         }
+
+        _myTween.Clear();
+        
+
+        _myTween.Add("easetype", "linear");
+        _myTween.Add("oncomplete", "OnComplete");
+        _myTween.Add("path", _waypoints.ToArray());
+        _myTween.Add("speed", pathSpeed);
+        
     }
     private void CreateIncomingPath()
     {
+
         Vector3 lastPoint = _waypoints[_waypoints.Count - 1];
         _waypoints.Clear();
-
+        
         Vector3 topSide = Camera.main.ViewportToWorldPoint(new Vector3(0,1,0));
-        _waypoints.Add(new Vector3(lastPoint.x, 0, topSide.z));
+        _waypoints.Add(new Vector3(lastPoint.x, 0, 43));
         _waypoints.Add(_originalPosition);
 
+        _myTween.Clear();
+
+        _myTween.Add("easetype", "linear");
+        _myTween.Add("path", _waypoints.ToArray());
+        _myTween.Add("oncomplete", "OnCompleteIncoming");
+        _myTween.Add("speed", pathSpeed);
+    }
+    private void OnComplete()
+    {
+        
+
+        Vector3 lastPoint = _waypoints[_waypoints.Count - 1];
+        Vector3 topSide = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0));
+        transform.position = new Vector3(lastPoint.x,0,topSide.z);
+        CreateIncomingPath();
+        iTween.MoveTo(gameObject, _myTween);
+    }
+    private void OnCompleteIncoming()
+    {
+        _finishedPath = true;
+        _isOnPath = false;
     }
     public void OnDrawGizmos()
     {
